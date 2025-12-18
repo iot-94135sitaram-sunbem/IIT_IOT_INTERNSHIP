604e1c813d07ab38e1dc07cfdcd64b913332d874
sitaramdeshmukh <sitaramzarikar123@gmail.com>
2025-12-18 19:32:16 +0530

    day6 assign

--- daily practis/day5/assignment5/Q1.py
+++ daily practis/day5/assignment5/Q1.py
@@ -1,37 +1,36 @@
 import math as m
-#1 - squareroot
+
 print(m.sqrt(16))
 
-#2 - degree to radian
+
 print(m.degrees(1))
 
-#3- radian to degree
+
 print(m.radians(180))
 
 import math as m
-#1 - squareroot
+
 print(m.sqrt(16))
 
-#2 - degree to radian
+
 print(m.degrees(1))
 
-#3- radian to degree
+
 print(m.radians(180))
 
-# 4 - cos and sine function
+
 print(m.cos(180))
 print(m.sin(0))
 
-#5 - log
+
 print(m.log10(10))
 
-#6 - factorial
 print(m.factorial(5))
 
 import os
 print("current working directory = ",os.getcwd())
 
-#creating directory
+
 dir="sunbeam(created using os module)"
 par_dir="D:\Temp"
 path=os.path.join(par_dir,dir)
@@ -43,13 +42,13 @@ print(os.getlogin())
 
 print(os.getppid())
 
-os.rename("os module.txt", "omkar.txt")
+os.rename("os module.txt", "sitaram.txt")
 
-file = open("omkar.txt", "w")
+file = open("sitaram.txt", "w")
 file.write("Hello Python\n")
 file.write("File handling using os module")
 file.close()
 
-file=open("omkar.txt","r")
+file=open("sitaram.txt","r")
 print(file.read())
 
